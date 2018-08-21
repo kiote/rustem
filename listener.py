f8929b89bb41b5ac2e388d720a4cccd0ca806ec0
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2018-08-21 14:31:01 +0300

    add greeting

--- listener.py
+++ listener.py
@@ -3,4 +3,6 @@ import snowboydecoder
 callbacks = [lambda: snowboydecoder.play_audio_file(snowboydecoder.DETECT_DONG)]
 
 detector = snowboydecoder.HotwordDetector("resources/models/Rustem.pmdl", sensitivity=0.5, audio_gain=1)
+print("Say something...")
 detector.start(detected_callback=callbacks)
+
