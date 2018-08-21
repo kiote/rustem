6cd4fb7e6f080fa18c46cae6802a5736eb499a85
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2018-08-21 14:19:07 +0300

    Add simple listener

--- listener.py
+++ listener.py
@@ -1,1 +1,7 @@
 import snowboydecoder
+
+def detect_callback():
+   print("Yes, Master!")
+
+detector = snowboydecoder.HotwordDetector("resources/models/Rustem.pmdl", sensitivity=0.5, audio_gain=1)
+detector.start(detect_callback)
