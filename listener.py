eebd3b289b62916c8eb10751e903662f0054d30f
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2018-08-21 14:27:06 +0300

    Add sound response

--- listener.py
+++ listener.py
@@ -1,7 +1,6 @@
 import snowboydecoder
 
-def detect_callback():
-   print("Yes, Master!")
+callbacks = [lambda: snowboydecoder.play_audio_file(snowboydecoder.DETECT_DONG)]
 
 detector = snowboydecoder.HotwordDetector("resources/models/Rustem.pmdl", sensitivity=0.5, audio_gain=1)
-detector.start(detect_callback)
+detector.start(detected_callback=callbacks)
