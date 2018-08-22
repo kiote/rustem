1cc96b272da2eed787f01a5f1deb7b85e9555fab
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2018-08-22 21:35:29 +0300

    lower sensitivity

--- listener.py
+++ listener.py
@@ -2,7 +2,7 @@ import snowboydecoder
 
 callbacks = [lambda: snowboydecoder.play_audio_file(snowboydecoder.DETECT_DONG)]
 
-detector = snowboydecoder.HotwordDetector("resources/models/Rustem.pmdl", sensitivity=0.5, audio_gain=1)
+detector = snowboydecoder.HotwordDetector("resources/models/Rustem.pmdl", sensitivity=0.42, audio_gain=1)
 print("Say something...")
 detector.start(detected_callback=callbacks)
 
